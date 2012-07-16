7a2a155930b1fb6a0f313600b6dc1931633945e9
Yanchuan Li <gillbates@google.com>
2012-07-16 09:50:29 +0800

    123
    Signed-off-by: Yanchuan Li <gillbates@google.com>

--- src/main/java/com/yanchuanli/games/pokr/test/Test.java
+++ src/main/java/com/yanchuanli/games/pokr/test/Test.java
@@ -1,27 +1,15 @@
 package com.yanchuanli.games.pokr.test;
 
-import com.yanchuanli.games.pokr.model.Player;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
-import org.json.JSONException;
-import org.json.JSONObject;
 import sun.misc.BASE64Encoder;
 
 import javax.net.ssl.HttpsURLConnection;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Copyright Candou.com
@@ -34,26 +22,26 @@ public class Test {
     private static Logger log = Logger.getLogger(Test.class);
     private static String AppStoreSandboxVerifyURL = "https://sandbox.itunes.apple.com/verifyReceipt";
     private static String AppStoreVerifyURL = "https://buy.itunes.apple.com/verifyReceipt";
-    private static  String data = "{\n" +
-                    "\t\"signature\" = \"AhwqqkESe15lSDEuFtM6NFP26HRcMEyEpJD6nnZH2l5IlPStP1KiTygQwaeaSTGnVFof4rcJn2ycb/wc/q7Z/me8E+MPJ7lKmksNSt7NuZuGUjDUFzc6MCHEPz2QJISqW27YwZ8Q+GAnYonlpXRPg2amI1e2ETMZGrlmGEd7L5X0AAADVzCCA1MwggI7oAMCAQICCGUUkU3ZWAS1MA0GCSqGSIb3DQEBBQUAMH8xCzAJBgNVBAYTAlVTMRMwEQYDVQQKDApBcHBsZSBJbmMuMSYwJAYDVQQLDB1BcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTEzMDEGA1UEAwwqQXBwbGUgaVR1bmVzIFN0b3JlIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MB4XDTA5MDYxNTIyMDU1NloXDTE0MDYxNDIyMDU1NlowZDEjMCEGA1UEAwwaUHVyY2hhc2VSZWNlaXB0Q2VydGlmaWNhdGUxGzAZBgNVBAsMEkFwcGxlIGlUdW5lcyBTdG9yZTETMBEGA1UECgwKQXBwbGUgSW5jLjELMAkGA1UEBhMCVVMwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMrRjF2ct4IrSdiTChaI0g8pwv/cmHs8p/RwV/rt/91XKVhNl4XIBimKjQQNfgHsDs6yju++DrKJE7uKsphMddKYfFE5rGXsAdBEjBwRIxexTevx3HLEFGAt1moKx509dhxtiIdDgJv2YaVs49B0uJvNdy6SMqNNLHsDLzDS9oZHAgMBAAGjcjBwMAwGA1UdEwEB/wQCMAAwHwYDVR0jBBgwFoAUNh3o4p2C0gEYtTJrDtdDC5FYQzowDgYDVR0PAQH/BAQDAgeAMB0GA1UdDgQWBBSpg4PyGUjFPhJXCBTMzaN+mV8k9TAQBgoqhkiG92NkBgUBBAIFADANBgkqhkiG9w0BAQUFAAOCAQEAEaSbPjtmN4C/IB3QEpK32RxacCDXdVXAeVReS5FaZxc+t88pQP93BiAxvdW/3eTSMGY5FbeAYL3etqP5gm8wrFojX0ikyVRStQ+/AQ0KEjtqB07kLs9QUe8czR8UGfdM1EumV/UgvDd4NwNYxLQMg4WTQfgkQQVy8GXZwVHgbE/UC6Y7053pGXBk51NPM3woxhd3gSRLvXj+loHsStcTEqe9pBDpmG5+sk4tw+GK3GMeEN5/+e1QT9np/Kl1nj+aBw7C0xsy0bFnaAd1cSS6xdory/CUvM6gtKsmnOOdqTesbp0bs8sn6Wqs0C9dgcxRHuOMZ2tm8npLUm7argOSzQ==\";\n" +
-                    "\t\"purchase-info\" = \"ewoJIm9yaWdpbmFsLXB1cmNoYXNlLWRhdGUtcHN0IiA9ICIyMDEyLTA3LTE1IDA2OjI3OjAzIEFtZXJpY2EvTG9zX0FuZ2VsZXMiOwoJIm9yaWdpbmFsLXRyYW5zYWN0aW9uLWlkIiA9ICIxMDAwMDAwMDUyODM0NTMwIjsKCSJidnJzIiA9ICIyMDEyMDcxNCI7CgkidHJhbnNhY3Rpb24taWQiID0gIjEwMDAwMDAwNTI4MzQ1MzAiOwoJInF1YW50aXR5IiA9ICIxIjsKCSJvcmlnaW5hbC1wdXJjaGFzZS1kYXRlLW1zIiA9ICIxMzQyMzU4ODIzMjU0IjsKCSJwcm9kdWN0LWlkIiA9ICIxMDAwMiI7CgkiaXRlbS1pZCIgPSAiNTQ1MTA0MjAzIjsKCSJiaWQiID0gImNvbS54eHgucmljaG1hbiI7CgkicHVyY2hhc2UtZGF0ZS1tcyIgPSAiMTM0MjM1ODgyMzI1NCI7CgkicHVyY2hhc2UtZGF0ZSIgPSAiMjAxMi0wNy0xNSAxMzoyNzowMyBFdGMvR01UIjsKCSJwdXJjaGFzZS1kYXRlLXBzdCIgPSAiMjAxMi0wNy0xNSAwNjoyNzowMyBBbWVyaWNhL0xvc19BbmdlbGVzIjsKCSJvcmlnaW5hbC1wdXJjaGFzZS1kYXRlIiA9ICIyMDEyLTA3LTE1IDEzOjI3OjAzIEV0Yy9HTVQiOwp9\";\n" +
-                    "\t\"environment\" = \"Sandbox\";\n" +
-                    "\t\"pod\" = \"100\";\n" +
-                    "\t\"signing-status\" = \"0\";\n" +
-                    "}";
+    private static String data = "{\n" +
+            "\t\"signature\" = \"AhwqqkESe15lSDEuFtM6NFP26HRcMEyEpJD6nnZH2l5IlPStP1KiTygQwaeaSTGnVFof4rcJn2ycb/wc/q7Z/me8E+MPJ7lKmksNSt7NuZuGUjDUFzc6MCHEPz2QJISqW27YwZ8Q+GAnYonlpXRPg2amI1e2ETMZGrlmGEd7L5X0AAADVzCCA1MwggI7oAMCAQICCGUUkU3ZWAS1MA0GCSqGSIb3DQEBBQUAMH8xCzAJBgNVBAYTAlVTMRMwEQYDVQQKDApBcHBsZSBJbmMuMSYwJAYDVQQLDB1BcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTEzMDEGA1UEAwwqQXBwbGUgaVR1bmVzIFN0b3JlIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MB4XDTA5MDYxNTIyMDU1NloXDTE0MDYxNDIyMDU1NlowZDEjMCEGA1UEAwwaUHVyY2hhc2VSZWNlaXB0Q2VydGlmaWNhdGUxGzAZBgNVBAsMEkFwcGxlIGlUdW5lcyBTdG9yZTETMBEGA1UECgwKQXBwbGUgSW5jLjELMAkGA1UEBhMCVVMwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMrRjF2ct4IrSdiTChaI0g8pwv/cmHs8p/RwV/rt/91XKVhNl4XIBimKjQQNfgHsDs6yju++DrKJE7uKsphMddKYfFE5rGXsAdBEjBwRIxexTevx3HLEFGAt1moKx509dhxtiIdDgJv2YaVs49B0uJvNdy6SMqNNLHsDLzDS9oZHAgMBAAGjcjBwMAwGA1UdEwEB/wQCMAAwHwYDVR0jBBgwFoAUNh3o4p2C0gEYtTJrDtdDC5FYQzowDgYDVR0PAQH/BAQDAgeAMB0GA1UdDgQWBBSpg4PyGUjFPhJXCBTMzaN+mV8k9TAQBgoqhkiG92NkBgUBBAIFADANBgkqhkiG9w0BAQUFAAOCAQEAEaSbPjtmN4C/IB3QEpK32RxacCDXdVXAeVReS5FaZxc+t88pQP93BiAxvdW/3eTSMGY5FbeAYL3etqP5gm8wrFojX0ikyVRStQ+/AQ0KEjtqB07kLs9QUe8czR8UGfdM1EumV/UgvDd4NwNYxLQMg4WTQfgkQQVy8GXZwVHgbE/UC6Y7053pGXBk51NPM3woxhd3gSRLvXj+loHsStcTEqe9pBDpmG5+sk4tw+GK3GMeEN5/+e1QT9np/Kl1nj+aBw7C0xsy0bFnaAd1cSS6xdory/CUvM6gtKsmnOOdqTesbp0bs8sn6Wqs0C9dgcxRHuOMZ2tm8npLUm7argOSzQ==\";\n" +
+            "\t\"purchase-info\" = \"ewoJIm9yaWdpbmFsLXB1cmNoYXNlLWRhdGUtcHN0IiA9ICIyMDEyLTA3LTE1IDA2OjI3OjAzIEFtZXJpY2EvTG9zX0FuZ2VsZXMiOwoJIm9yaWdpbmFsLXRyYW5zYWN0aW9uLWlkIiA9ICIxMDAwMDAwMDUyODM0NTMwIjsKCSJidnJzIiA9ICIyMDEyMDcxNCI7CgkidHJhbnNhY3Rpb24taWQiID0gIjEwMDAwMDAwNTI4MzQ1MzAiOwoJInF1YW50aXR5IiA9ICIxIjsKCSJvcmlnaW5hbC1wdXJjaGFzZS1kYXRlLW1zIiA9ICIxMzQyMzU4ODIzMjU0IjsKCSJwcm9kdWN0LWlkIiA9ICIxMDAwMiI7CgkiaXRlbS1pZCIgPSAiNTQ1MTA0MjAzIjsKCSJiaWQiID0gImNvbS54eHgucmljaG1hbiI7CgkicHVyY2hhc2UtZGF0ZS1tcyIgPSAiMTM0MjM1ODgyMzI1NCI7CgkicHVyY2hhc2UtZGF0ZSIgPSAiMjAxMi0wNy0xNSAxMzoyNzowMyBFdGMvR01UIjsKCSJwdXJjaGFzZS1kYXRlLXBzdCIgPSAiMjAxMi0wNy0xNSAwNjoyNzowMyBBbWVyaWNhL0xvc19BbmdlbGVzIjsKCSJvcmlnaW5hbC1wdXJjaGFzZS1kYXRlIiA9ICIyMDEyLTA3LTE1IDEzOjI3OjAzIEV0Yy9HTVQiOwp9\";\n" +
+            "\t\"environment\" = \"Sandbox\";\n" +
+            "\t\"pod\" = \"100\";\n" +
+            "\t\"signing-status\" = \"0\";\n" +
+            "}";
 
-    public static void main(String[] args) throws IOException, UnsupportedEncodingException, JSONException {
+    public static void main(String[] args) throws IOException {
 
         verifyIAP();
     }
 
-    public static void verifyIAP() throws JSONException {
+    public static void verifyIAP() {
         HttpURLConnection.setFollowRedirects(false);
         BASE64Encoder encoder = new BASE64Encoder();
         String encodedBytes = encoder.encodeBuffer(data.getBytes());
 
 
-        String json="{\"receipt-data\":\""+encodedBytes+"\"}";
+        String json = "{\"receipt-data\":\"" + encodedBytes + "\"}";
         HttpURLConnection con = null;
         try {
 
