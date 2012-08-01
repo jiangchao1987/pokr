32d5034f5ae8793aee9ef1e1cffb2a46c3a19142
Yanchuan Li <gillbates@google.com>
2012-08-01 10:45:53 +0800

    123
    Signed-off-by: Yanchuan Li <gillbates@google.com>

--- src/main/java/com/yanchuanli/games/pokr/event/Event.java
+++ src/main/java/com/yanchuanli/games/pokr/event/Event.java
@@ -7,4 +7,47 @@ package com.yanchuanli.games.pokr.event;
  * Date: 12-7-27
  */
 public class Event {
+    private String udid;
+    private int timestamp;
+    private int type;
+    private String info;
+
+    public Event(String udid, int timestamp, int type, String info) {
+        this.udid = udid;
+        this.timestamp = timestamp;
+        this.type = type;
+        this.info = info;
+    }
+
+    public String getUdid() {
+        return udid;
+    }
+
+    public void setUdid(String udid) {
+        this.udid = udid;
+    }
+
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(int timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
 }
