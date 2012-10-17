d26f8ebd73d88654d52613232a97d4c32e0b2e88
Yanchuan Li <mail@yanchuanli.com>
2012-10-17 13:07:13 +0800

    123
    Signed-off-by: Yanchuan Li <mail@yanchuanli.com>

--- src/main/java/com/yanchuanli/games/pokr/util/MongoDB.java
+++ src/main/java/com/yanchuanli/games/pokr/util/MongoDB.java
@@ -2,9 +2,6 @@ package com.yanchuanli.games.pokr.util;
 
 public class MongoDB {
 	
-	public static final String DBHOST = "192.168.8.226";
-	public static final int DBPORT = 27017;
-	
 	public static final String DBNAME = "texas";
 	public static final String COLL_USER = "user";
 	public static final String COLL_ROOM = "room";
