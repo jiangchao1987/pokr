4d1562198625a4e2f67e186a1188dc3d45433a6c
jiangchao1987 <jiangchao1987@gmail.com>
2012-10-17 18:05:46 +0800

    db config
    Signed-off-by: jiangchao1987 <jiangchao1987@gmail.com>

--- src/main/java/com/yanchuanli/games/pokr/util/MongoDB.java
+++ src/main/java/com/yanchuanli/games/pokr/util/MongoDB.java
@@ -1,9 +1,14 @@
 package com.yanchuanli.games.pokr.util;
 
+import com.yanchuanli.games.pokr.conf.Configure;
+
 public class MongoDB {
 	
-	public static final String DBHOST = "192.168.1.177";
-	public static final int DBPORT = 27017;
+//	public static final String DBHOST = "192.168.1.177";
+//	public static final String DBHOST = "114.112.62.226";
+	public static final String DBHOST = Configure.getProperty("db_host");
+//	public static final int DBPORT = 27017;
+	public static final int DBPORT = Integer.parseInt(Configure.getProperty("db_port"));
 	
 	public static final String DBNAME = "texas";
 	public static final String COLL_USER = "user";
