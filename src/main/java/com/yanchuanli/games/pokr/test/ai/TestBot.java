a3638363226a18a667724a22b303243796835062
Yanchuan Li <mail@yanchuanli.com>
2012-10-27 19:10:54 +0800

    123
    Signed-off-by: Yanchuan Li <mail@yanchuanli.com>

--- src/main/java/com/yanchuanli/games/pokr/test/ai/TestBot.java
+++ src/main/java/com/yanchuanli/games/pokr/test/ai/TestBot.java
@@ -10,7 +10,7 @@ import com.yanchuanli.games.pokr.conf.Configure;
  */
 public class TestBot {
     public static void main(String[] args) {
-        Bot bot = new Bot("d", "d123", Integer.parseInt(Configure.getProperty("10111")));
+        Bot bot = new Bot("e", "e123", Integer.parseInt(Configure.getProperty("room_bot")));
         Thread botThread = new Thread(bot);
         botThread.start();
     }
