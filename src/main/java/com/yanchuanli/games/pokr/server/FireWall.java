bc5c55768e7e83eea54a98030d8fc41042037fcb
Yanchuan Li <mail@yanchuanli.com>
2012-11-06 17:29:39 +0800

    213
    Signed-off-by: Yanchuan Li <mail@yanchuanli.com>

--- src/main/java/com/yanchuanli/games/pokr/server/FireWall.java
+++ src/main/java/com/yanchuanli/games/pokr/server/FireWall.java
@@ -1,6 +1,7 @@
 package com.yanchuanli.games.pokr.server;
 
 import com.yanchuanli.games.pokr.util.Memory;
+import org.apache.log4j.Logger;
 import org.apache.mina.core.session.IoSession;
 
 /**
@@ -9,11 +10,25 @@ import org.apache.mina.core.session.IoSession;
  * Date: 11/6/12
  */
 public class FireWall {
+
+    private static Logger log = Logger.getLogger(FireWall.class);
+
     public static boolean validate(IoSession session) {
         boolean result = Memory.sessionsOnServer.containsKey(String.valueOf(session.getId())) || Memory.adminSessionsOnServer.containsKey(String.valueOf(session.getId()));
+
+        if (result) {
+            result = validateIP(session);
+        }
+
         if (!result) {
             session.close(true);
         }
         return result;
     }
+
+    private static boolean validateIP(IoSession session) {
+        //log.info("incomming client : " + session.getRemoteAddress());
+        return true;
+    }
+
 }
