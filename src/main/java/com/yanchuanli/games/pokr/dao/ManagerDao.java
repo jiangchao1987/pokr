de6f1e775d40c5ad4e7a39d4c6490996daec1075
jiangchao1987 <jiangchao1987@gmail.com>
2012-11-06 16:17:51 +0800

    valid manager Signed-off-by: jiangchao1987 <jiangchao1987@gmail.com>

---
+++ src/main/java/com/yanchuanli/games/pokr/dao/ManagerDao.java
@@ -0,0 +1,28 @@
+package com.yanchuanli.games.pokr.dao;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.yanchuanli.games.pokr.util.MongoDB;
+import com.yanchuanli.games.pokr.util.MongoDBFactory;
+
+public class ManagerDao {
+	
+	public boolean validateAdmin(String name, String pwd) {
+		boolean result = false;
+		
+		DBCollection coll = MongoDBFactory.getCollection(MongoDB.DBNAME,
+                MongoDB.COLL_MANAGER);
+        BasicDBObject query = new BasicDBObject();
+        query.put("name", name);
+        query.put("pwd", pwd);	
+        DBCursor cur = coll.find(query);
+        
+        while(cur.hasNext()) {
+        	result = true;
+        }
+        
+        return result;
+	}
+
+}
--- src/main/java/com/yanchuanli/games/pokr/util/MongoDB.java
+++ src/main/java/com/yanchuanli/games/pokr/util/MongoDB.java
@@ -14,5 +14,6 @@ public class MongoDB {
 	public static final String COLL_USER = "user";
 	public static final String COLL_ROOM = "room";
 	public static final String COLL_EVENT = "event";
+	public static final String COLL_MANAGER = "manager";
 	
 }
